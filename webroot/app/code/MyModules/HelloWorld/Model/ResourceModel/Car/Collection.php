9d8b1964c6788b294a78205e0b5c5c1562964e79
emonShimoul <emonshimoul@gmail.com>
2022-11-06 00:50:48 +0600

    write code for collection

--- webroot/app/code/MyModules/HelloWorld/Model/ResourceModel/Car/Collection.php
+++ webroot/app/code/MyModules/HelloWorld/Model/ResourceModel/Car/Collection.php
@@ -2,7 +2,14 @@
 
 namespace MyModules\HelloWorld\Model\ResourceModel\Car;
 
-class Collection
-{
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use MyModules\HelloWorld\Model\Car as Model;
+use MyModules\HelloWorld\Model\ResourceModel\Car as ResourceModel;
 
+class Collection extends AbstractCollection
+{
+    protected function _construct()
+    {
+        $this->_init(Model::class, ResourceModel::class);
+    }
 }
