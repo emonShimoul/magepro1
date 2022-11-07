a97adbd34ff8e92c213ae8eb9a79905cdb930e31
emonShimoul <emonshimoul@gmail.com>
2022-11-08 01:33:16 +0600

    slight changes

--- webroot/app/code/Pulsestorm/TutorialObjectPreference/Model/Messenger3.php
+++ webroot/app/code/Pulsestorm/TutorialObjectPreference/Model/Messenger3.php
@@ -1,9 +1,9 @@
 <?php
 namespace Pulsestorm\TutorialObjectPreference\Model;
 class Messenger3
-{    
+{
     public function getMessage()
     {
-        return 'Injection?  We don\'t need no stinking injection!';
+        return 'Injection?  We don\'t need no stinking injection!!!!';
     }
 }
